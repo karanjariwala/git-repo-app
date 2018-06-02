0829dbaf0daddc5ac19361635887a6e02e7b8995
karan jariwala <karan.j@practo.com>
2018-06-02 20:27:30 +0530

    Connecting cardList

--- src/RepoSearchPage/Components/CardList.jsx
+++ src/RepoSearchPage/Components/CardList.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import {connect } from 'react-redux';
 
 const Container=styled.div`
 height: 80vh;
 width: 100%;
-
-
 `
-const CardList = () => <Container> cards coming soon </Container>
+const CardList = ({ orderArray, repositories }) => (<Container>
+     {orderArray ? orderArray.map((id)=><div key={id}>{repositories[id].full_name}</div>) : ''}
+     </Container>)
 
 
-export default CardList;
+const mapStateToProps = (state) => {
+  const {result, entities }= state.repository.repositoryData;
+  return {
+      orderArray: result,
+      repositories: entities
+  }
+}
+
+export default connect(mapStateToProps)(CardList);
