85d1d78f4a8e6c6c71bb17274182e2f26ff3e14b
karan jariwala <karan.j@practo.com>
2018-06-02 17:29:39 +0530

    wrapper over axios for api req

---
+++ src/Common/utils/apiClient.js
@@ -0,0 +1,44 @@
+/* global contextPath */
+import qs from 'qs';
+import axios from 'axios';
+
+
+
+const TIMEOUT = 600000;
+function apiClient(
+  url = '',
+  httpMethod = 'get',
+  params = {},
+  data = {},
+  headers = {},
+  cancelRequestToken = '',
+  baseURL,
+) {
+  return axios({
+    url,
+    baseURL: baseURL ? baseURL :'https://api.github.com/',
+    timeout: TIMEOUT,
+    method: httpMethod,
+    data,
+    headers,
+    params,
+    cancelToken: cancelRequestToken,
+    paramsSerializer(param) {
+      return qs.stringify(param, { arrayFormat: 'repeat' });
+    },
+  }).catch(error => {
+    if (axios.isCancel(error)) {
+      throw new Error('Request cancelled in axios due to redundancy');
+    } else if (!error.response) {
+      throw new Error('Network Error');
+    } else {
+      const errorEvent = new CustomEvent('apiError', {
+        detail: error.response.data,
+      });
+      window.dispatchEvent(errorEvent);
+      throw error;
+    }
+  });
+}
+
+export default apiClient;
