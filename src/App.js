91c5c40b4e0d94efb7715fee81b01b6c1e193320
karan jariwala <karan.j@practo.com>
2018-06-02 14:11:15 +0530

    create-react app setup with redux, react-router and thunk

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+
+const Home=()=><div> home</div>
+const About =()=><div>About</div>
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
+
+export default App
