cdb1339e50cebcf77e0c05f418b2149ddc15ae54
karan jariwala <karan.j@practo.com>
2018-06-02 17:31:24 +0530

    adding repository state slice

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import { routerReducer } from 'react-router-redux';
+import repositorySearchPage from './RepoSearchPage/reducer';
 
 export default combineReducers({
-  routing: routerReducer
-})
+  routing: routerReducer,
+  repository:repositorySearchPage, 
+});
