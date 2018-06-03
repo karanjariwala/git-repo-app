bad402b26b070ccc492ffb2603a9f7147b6e402e
karan jariwala <karan.j@practo.com>
2018-06-03 19:51:00 +0530

    normalize basic schema added for contri

--- src/Common/utils/normalize.js
+++ src/Common/utils/normalize.js
@@ -5,4 +5,13 @@ const repositorySchema = new schema.Entity('repositories');
 // or use shorthand syntax:
 const repositoryListSchema = [ repositorySchema ];
 
-export const normalizedData = (originalData) => normalize(originalData, repositoryListSchema);
+export const normalizedDataRepositories = (originalData) => normalize(originalData, repositoryListSchema);
+
+
+
+const contributorSchema = new schema.Entity('contributors');
+
+// or use shorthand syntax:
+const contributorListSchema = [ contributorSchema ];
+
+export const normalizedDataContributors = (originalData) => normalize(originalData, contributorListSchema);
