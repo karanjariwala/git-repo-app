7fd31c2fa0fb44a8b8f059b2c50cd4db70263e9b
karan jariwala <karan.j@practo.com>
2018-06-03 19:52:35 +0530

    Custom Rederer addded for list as a render prop

---
+++ src/ContributorsPage/Components/CustomCardRenderer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from '../../Common/Components/Card/index';
+import Button from '../../Common/Components/Button';
+import styled from 'styled-components';
+
+const CustomCardWrapper= styled.div`
+display:flex;
+justify-content:space-around;
+align-items: center;
+`
+
+const Image= styled.img`
+border-radius:50%;
+`
+
+const CustomCardRenderer = (props) => {
+    const { contributor, showMore }= props;
+    const { avatar_url, login, contributions } = contributor;
+
+    return <CustomCardWrapper>
+                <Image height="42" width="42" src={avatar_url} />
+                <Button link align={'center'}> {login} </Button>
+                <div> {contributions} Contributions </div>
+            </CustomCardWrapper>
+
+}
+
+
+
+export default CustomCardRenderer;
