d3b496fbdee5ba0462dab472e8c09b7fb37e51f9
karan jariwala <karan.j@practo.com>
2018-06-03 19:51:16 +0530

    Main page contributors page

---
+++ src/ContributorsPage/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Actions } from './Actions';
+import ContributorsList from './Components/ContributorList';
+
+
+class ContributorsPage extends React.Component{
+    componentDidMount(){
+        const { match, fetchContributors} = this.props;
+        fetchContributors(match.params.accountName, match.params.repositoryName);
+    }
+
+    render(){
+        return  <ContributorsList /> 
+    }
+}
+
+
+
+
+const mapDispatchToProps = dispatch => ({
+    fetchContributors: (accountName, repositoryName) => dispatch(Actions.fetchContributors(accountName, repositoryName))
+})
+
+export default connect(null, mapDispatchToProps)(ContributorsPage);
