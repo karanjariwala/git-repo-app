73ceec965369ad127f0434422b4a598c04e1cca7
karan jariwala <karan.j@practo.com>
2018-06-03 19:48:54 +0530

    adding contributor saga and reducer to respective root reducers

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux';
 import repositorySearchPage from './RepoSearchPage/reducer';
+import contributorSearchPage from './ContributorsPage/reducer';
 
 export default combineReducers({
   routing: routerReducer,
   repository:repositorySearchPage, 
+  contributors:contributorSearchPage,
 });
--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,11 @@
 import { takeEvery, all } from 'redux-saga/effects';
 import repositorySaga  from './RepoSearchPage/saga';
-
+import  contributorsSaga from './ContributorsPage/saga';
 
 export default function* rootSaga() {
     yield all([
-        repositorySaga(),    
+        repositorySaga(),
+        contributorsSaga(),    
     ])
   }
 
