38883fd8599af0797f08fed408bfd73700a4f7c4
karan jariwala <karan.j@practo.com>
2018-06-05 01:39:31 +0530

    very Basic Error Handler Component

---
+++ src/Common/Components/ErrorHandler.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+import styled from 'styled-components';
+
+const ErrorContainer= styled.div`
+color: red;
+font-size: 2em;
+padding: 40px;
+`
+
+// This is just a basic error handler. 
+
+const  ErrorHandler = ({ error = false , children }) => {
+    if(error){
+        return <ErrorContainer> API ERROR : <br /> <br /> {error} </ErrorContainer>
+    }
+    if(children.length){
+        return [...children]
+    }
+    return children
+}
+
+
+export default ErrorHandler;
